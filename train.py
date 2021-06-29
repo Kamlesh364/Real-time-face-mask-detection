0e52ff2110a9fc46ccdba53c87c02df2c3b6ba1c
Kamelsh Kumar <patelkamleshpatel364@gmail.com>
2021-06-29 15:27:38 +0530

    changing to train.py

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+#!usr/bin/env python3
+
 # Import Keras modules and its important APIs
 import keras
 from keras.layers import Dense, Conv2D, BatchNormalization, Activation
@@ -181,57 +183,3 @@ history = model.fit(train_generator,
                           epochs=5,
                           validation_data=validation_generator,
                           callbacks=[checkpoint])
-
-import cv2
-import numpy as np
-from keras.models import load_model
-model=load_model("./model2-005.model")
-
-labels_dict={0:'without mask',1:'mask'}
-color_dict={0:(0,0,255),1:(0,255,0)}
-
-size = 4
-webcam = cv2.VideoCapture(0) # launching the webcam
-
-# loading the xml file to detect faces in a video stream
-classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-while True:
-    (rval, im) = webcam.read()
-    im=cv2.flip(im,1,1) #Flipping to act as a mirror
-
-    # Resizing the image to speed up detection
-    mini = cv2.resize(im, (im.shape[1] // size, im.shape[0] // size))
-
-    # detecting MultiScale-faces 
-    faces = classifier.detectMultiScale(mini)
-
-    # Draw rectangles around each face
-    for f in faces:
-        (x, y, w, h) = [v * size for v in f] #Scaling the shapesize backup
-        #Saving just the rectangle faces in SubRecFaces
-        face_img = im[y:y+h, x:x+w]
-        resized=cv2.resize(face_img,(150,150))
-        normalized=resized/255.0
-        reshaped=np.reshape(normalized,(1,150,150,3))
-        reshaped = np.vstack([reshaped])
-        result=model.predict(reshaped)
-        #print(result)
-        
-        label=np.argmax(result,axis=1)[0]
-      
-        cv2.rectangle(im,(x,y),(x+w,y+h),color_dict[label],2)
-        cv2.rectangle(im,(x,y-40),(x+w,y),color_dict[label],-1)
-        cv2.putText(im, labels_dict[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
-        
-        # Showing the image
-        cv2.imshow('LIVE',   im)
-        key = cv2.waitKey(10)
-        # press 'Esc' to break the loop
-        if key == 27:
-            break
-# Stopping video
-webcam.release()
-
-# Closing all started windows
-cv2.destroyAllWindows()
