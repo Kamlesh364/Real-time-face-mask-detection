aff256a1af2ddd3c8f89478da5e9fd053a3c0f95
Kamelsh Kumar <patelkamleshpatel364@gmail.com>
2021-06-29 15:04:00 +0530

    create detection.py

---
+++ detection.py
@@ -0,0 +1,237 @@
+# Import Keras modules and its important APIs
+import keras
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from keras.layers import AveragePooling2D, Input, Flatten
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l2
+from keras import backend as K
+from keras.models import Model
+from keras.datasets import cifar10
+import numpy as np
+import os
+
+# Setting Training Hyperparameters
+batch_size = 32  # original ResNet paper uses batch_size = 128 for training
+epochs = 200
+data_augmentation = True
+num_classes = 10
+
+# Data Preprocessing 
+subtract_pixel_mean = True
+n = 3
+
+depth = n * 9 + 2
+
+# Setting LR for different number of Epochs
+def lr_schedule(epoch):
+    lr = 1e-3
+    if epoch > 180:
+        lr *= 0.5e-3
+    elif epoch > 160:
+        lr *= 1e-3
+    elif epoch > 120:
+        lr *= 1e-2
+    elif epoch > 80:
+        lr *= 1e-1
+    print('Learning rate: ', lr)
+    return lr
+  
+ # Basic ResNet Building Block
+def resnet_layer(inputs,num_filters = 16,kernel_size = 3, strides = 1, activation ='relu', conv_first=False, batch_normalization = True):
+
+    conv = Conv2D(num_filters,
+                  kernel_size = kernel_size,
+                  strides = strides,
+                  padding ='same',
+                  kernel_initializer ='he_normal',
+                  kernel_regularizer = l2(1e-4))
+  
+    x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+        x = conv(x)
+    return x
+  
+  # ResNet V2 architecture
+def resnet_v2(input_shape, depth, num_classes = 2):
+    if (depth - 2) % 9 != 0:
+        raise ValueError('depth should be 9n + 2 (eg 56 or 110 in [b])')
+    # Start model definition.
+    num_filters_in = 16
+    num_res_blocks = int((depth - 2) / 9)
+  
+    inputs = Input(shape = input_shape)
+    # v2 performs Conv2D with BN-ReLU on input before splitting into 2 paths
+    x = resnet_layer(inputs = inputs,
+                     num_filters = num_filters_in,
+                     conv_first = True)
+  
+    # Instantiate the stack of residual units
+    for stage in range(3):
+        for res_block in range(num_res_blocks):
+            activation = 'relu'
+            batch_normalization = True
+            strides = 1
+            if stage == 0:
+                num_filters_out = num_filters_in * 4
+                if res_block == 0:  # first layer and first stage
+                    activation = None
+                    batch_normalization = False
+            else:
+                num_filters_out = num_filters_in * 2
+                if res_block == 0:  # first layer but not first stage
+                    strides = 2    # downsample
+  
+            # bottleneck residual unit
+            y = resnet_layer(inputs = x,
+                         num_filters = num_filters_in,
+                         kernel_size = 1,
+                         strides = strides,
+                         activation = activation,
+                         batch_normalization = batch_normalization,
+                         conv_first = False)
+            y = resnet_layer(inputs = y,
+                             num_filters = num_filters_in,
+                             conv_first = False)
+            y = resnet_layer(inputs = y,
+                             num_filters = num_filters_out,
+                             kernel_size = 1,
+                             conv_first = False)
+            if res_block == 0:
+                # linear projection residual shortcut connection to match
+                # changed dims
+                x = resnet_layer(inputs = x,
+                                 num_filters = num_filters_out,
+                                 kernel_size = 1,
+                                 strides = strides,
+                                 activation = None,
+                                 batch_normalization = False)
+            x = keras.layers.add([x, y])
+  
+        num_filters_in = num_filters_out
+  
+    # Add classifier on top.
+    # v2 has BN-ReLU before Pooling
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = AveragePooling2D(pool_size = 8)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes,
+                    activation ='softmax',
+                    kernel_initializer ='he_normal')(y)
+
+    # Instantiate model.
+    model = Model(inputs = inputs, outputs = outputs)
+    return model
+  
+  TRAINING_DIR = "./Dataset/train"
+train_datagen = ImageDataGenerator(rescale=1.0/255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+
+train_generator = train_datagen.flow_from_directory(TRAINING_DIR, 
+                                                    batch_size=10, 
+                                                    target_size=(150, 150))
+VALIDATION_DIR = "./Dataset/test"
+
+validation_datagen = ImageDataGenerator(rescale=1.0/255)
+
+validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR, 
+                                                         batch_size=10, 
+                                                         target_size=(150, 150))
+
+model = resnet_v2(input_shape = (150, 150,3), depth = depth)
+
+model.compile(loss ='categorical_crossentropy',
+              optimizer = Adam(learning_rate = lr_schedule(0)),
+              metrics =['accuracy'])
+
+# model.summary()
+
+# Prepare callbacks for model saving and for learning rate adjustment.
+checkpoint = ModelCheckpoint('model2-{epoch:03d}.model',monitor='val_loss',verbose=0,save_best_only=True,mode='auto')
+  
+lr_scheduler = LearningRateScheduler(lr_schedule)
+  
+lr_reducer = ReduceLROnPlateau(factor = np.sqrt(0.1),
+                               cooldown = 0,
+                               patience = 5,
+                               min_lr = 0.5e-6)
+  
+callbacks = [checkpoint, lr_reducer, lr_scheduler]
+
+history = model.fit(train_generator,
+                          epochs=5,
+                          validation_data=validation_generator,
+                          callbacks=[checkpoint])
+
+import cv2
+import numpy as np
+from keras.models import load_model
+model=load_model("./model2-005.model")
+
+labels_dict={0:'without mask',1:'mask'}
+color_dict={0:(0,0,255),1:(0,255,0)}
+
+size = 4
+webcam = cv2.VideoCapture(0) # launching the webcam
+
+# loading the xml file to detect faces in a video stream
+classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+    (rval, im) = webcam.read()
+    im=cv2.flip(im,1,1) #Flipping to act as a mirror
+
+    # Resizing the image to speed up detection
+    mini = cv2.resize(im, (im.shape[1] // size, im.shape[0] // size))
+
+    # detecting MultiScale-faces 
+    faces = classifier.detectMultiScale(mini)
+
+    # Draw rectangles around each face
+    for f in faces:
+        (x, y, w, h) = [v * size for v in f] #Scaling the shapesize backup
+        #Saving just the rectangle faces in SubRecFaces
+        face_img = im[y:y+h, x:x+w]
+        resized=cv2.resize(face_img,(150,150))
+        normalized=resized/255.0
+        reshaped=np.reshape(normalized,(1,150,150,3))
+        reshaped = np.vstack([reshaped])
+        result=model.predict(reshaped)
+        #print(result)
+        
+        label=np.argmax(result,axis=1)[0]
+      
+        cv2.rectangle(im,(x,y),(x+w,y+h),color_dict[label],2)
+        cv2.rectangle(im,(x,y-40),(x+w,y),color_dict[label],-1)
+        cv2.putText(im, labels_dict[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+        
+        # Showing the image
+        cv2.imshow('LIVE',   im)
+        key = cv2.waitKey(10)
+        # press 'Esc' to break the loop
+        if key == 27:
+            break
+# Stopping video
+webcam.release()
+
+# Closing all started windows
+cv2.destroyAllWindows()
